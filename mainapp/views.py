5f0e330b1eff59eb91f76b7f5527d63ae63353d9
pappicandy123 <pappicandy@gmail.com>
2022-11-03 14:11:42 +0100

    Deploy update

--- mainapp/views.py
+++ mainapp/views.py
@@ -285,7 +285,7 @@ def pay(request):
     if request.method == 'POST':
         api_key = 'sk_test_2fd9254f6f947de8430053723f33d0d763b3b696'
         curl = ' https://api.paystack.co/transaction/initialize'
-        cburl = 'http://52.91.188.137/callback'
+        cburl = 'http://54.197.38.200/callback'
         ref = str(uuid.uuid4())
         amount = float(request.POST['total']) * 100
         cartno = request.POST['cartno']
